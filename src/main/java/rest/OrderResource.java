8aff1722e68429c9b6f80d1690c0c1bb6b7b1d1f
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-04-09 10:32:10 -0300

    Removed unused imports

--- src/main/java/rest/OrderResource.java
+++ src/main/java/rest/OrderResource.java
@@ -1,11 +1,8 @@
 package rest;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import model.dto.SaveOrderDTO;
 import org.eclipse.microprofile.reactive.messaging.Channel;
