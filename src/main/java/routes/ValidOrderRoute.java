95632547fe75e148df5306a98b848b35f8b27ce7
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-04-10 23:10:46 -0300

    Adding dead letter channel for error handling

--- src/main/java/routes/ValidOrderRoute.java
+++ src/main/java/routes/ValidOrderRoute.java
@@ -4,6 +4,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import model.Order;
 import model.dto.SaveOrderDTO;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import service.OrderService;
 
@@ -15,6 +16,13 @@ public class ValidOrderRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        errorHandler(
+            deadLetterChannel("kafka:dlq-orders?brokers=localhost:9092")
+                    .maximumRedeliveries(3)
+                    .redeliveryDelay(2000)
+                    .retryAttemptedLogLevel(LoggingLevel.WARN)
+        );
+
         from("kafka:valid-order?brokers=localhost:9092")
                 .routeId("valid-order-route")
                 .log("Raw message: ${body}")
